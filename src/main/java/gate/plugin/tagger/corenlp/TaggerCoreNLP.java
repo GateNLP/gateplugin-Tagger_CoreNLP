446e0c3abea08b10cc96b0027548f49f4e5adea8
Johann Petrak <johann.petrak@gmail.com>
2016-08-30 17:07:31 +0100

    First working version.

--- src/main/java/gate/plugin/tagger/corenlp/TaggerCoreNLP.java
+++ src/main/java/gate/plugin/tagger/corenlp/TaggerCoreNLP.java
@@ -8,6 +8,8 @@
  */
 package gate.plugin.tagger.corenlp;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import gate.Annotation;
 import gate.AnnotationSet;
 import gate.Controller;
@@ -25,6 +27,13 @@ import java.util.List;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 /**
@@ -77,8 +86,8 @@ public class TaggerCoreNLP extends AbstractDocumentProcessor {
     return outputAnnotationSet;
   }
  
-  public String serverUrl = "http://127.0.0.1:9000";
-  @CreoleParameter(comment = "The CoreNLP server address",defaultValue = "http:127.0.0.1:9000")
+  public String serverUrl = "http://127.0.0.1:9000/";
+  @CreoleParameter(comment = "The CoreNLP server address",defaultValue = "http://127.0.0.1:9000/")
   @RunTime
   @Optional
   public void setServerUrl(String val) {
@@ -128,10 +137,131 @@ public class TaggerCoreNLP extends AbstractDocumentProcessor {
     return document;
   }
   
+  // This sends some text over to the CoreNLP server, gets back the result
+  // as JSON and uses the JSON data to create annotations.
   public void processSpan(Document document, String text, int spanOffset) {
+    AnnotationSet outset = document.getAnnotations(getOutputAnnotationSet());
+    // this is the data we will send over to the server
+    Map data4json = new HashMap<String,Object>();
+    FeatureMap props = getProperties();
+    if(props == null || props.isEmpty()) {
+      data4json.put("annotators","tokenize, ssplit, pos, ner, parse");      
+      data4json.put("tokenize.language","en");
+    } else {
+      data4json.putAll(props);
+    }
+    String json = null;
+    ObjectMapper mapper = new ObjectMapper();
+    try {
+      json = mapper.writeValueAsString(data4json);
+    } catch (JsonProcessingException ex) {
+      throw new GateRuntimeException("Could not convert parameters to json",ex);
+    }
+    //System.err.println("GOT JSON: "+json);
     
+    HttpResponse<String> response;
+    try {
+      response = Unirest.post(serverUrl)
+              .header("accept","application/json")
+              .queryString("properties", json)
+              .body(text)
+              .asString();
+    } catch (UnirestException ex) {
+      throw new GateRuntimeException("Exception when connecting to the server",ex);
+    }
 
-  }
+    // The response should be either OK and JSON or not OK and an error message
+    int status = response.getStatus();
+    if(status != 200) {
+      throw new GateRuntimeException("Response von server is NOK, status="+status+" msg="+response.getBody());
+    }
+    //System.err.println("Got response, status is OK, data is: "+response.getBody());
+    Map responseMap = null;
+    try {
+      // Parse the json
+      responseMap = mapper.readValue(response.getBody(), HashMap.class);
+    } catch (IOException ex) {
+      throw new GateRuntimeException("Could not read the response",ex);
+    }
+    // The map may contain the following fields, depending on what we wanted CoreNLP to do:
+    // sentences: an array of maps, each inner maps has
+    //   index: int
+    //   parse: could be the constant string "SENTENCE_SKIPPED_OR_UNPARSABLE" or the parse
+    //     in conventional format: (ROOT\n  (S\n    (NP (NNP George)
+    //   tokens: an array of tokens of the following form
+    //      index: int
+    //      word: String
+    //      characterOffsetEnd: int
+    //      ner: entity type or "O" 
+    //      characterOffsetBegin: int
+    //      lemma: String
+    //      originalText: String (not sure what is the difference to word)
+    //      pos: the pos tag, String
+    //      before/after: not sure what these are for, always empty in my tests
+    //   Offsets continue accross sentences and seem to count whitespace correctly.
+    
+    List<Map<String,Object>> sentences = (List<Map<String,Object>>)responseMap.get("sentences");
+    for(Map<String,Object> sentence : sentences) {
+      // get the tokens
+      List<Map<String,Object>> tokens = (List<Map<String,Object>>)sentence.get("tokens");
+      // we need to create annotations for ners across tokens, so we remember
+      // the last type and last starting offset. If the type is "O" there is no
+      // current NE. We create a new annotation whenever we get a different type and the 
+      // last type was not an O plus after the sentence when the last type was 
+      // not an O
+      String lastType = "O";
+      int lastBegin = -1;
+      int lastEnd = -1;
+      // we also remember the begin offset of the first token as sentenceBegin
+      int sentenceBegin = -1;
+      int sentenceEnd = -1;
+      String ner = null;
+      for(Map<String,Object> token : tokens) {
+        int begin = (Integer)token.get("characterOffsetBegin");
+        int end = (Integer)token.get("characterOffsetEnd");
+        int index = (Integer)token.get("index");
+        ner = (String)token.get("ner");
+        String pos = (String)token.get("pos");
+        String lemma = (String)token.get("lemma");
+        
+        if(sentenceBegin == -1) {
+          sentenceBegin = begin;
+        }
+        sentenceEnd = end;
+        // create the token annotation
+        FeatureMap fm = Factory.newFeatureMap();
+        if(pos!=null) fm.put("category", pos);
+        if(lemma!=null) fm.put("root",pos);
+        fm.put("index",index);
+        if(ner!=null) fm.put("ner",ner);
+        Utils.addAnn(outset, spanOffset+begin, spanOffset+end, "Token", fm);
+        
+        if(ner != null) {
+          // we started a NE in the past and now have either a different one or an O
+          // we reset the last NE info.
+          if(!lastType.equals("O") && !lastType.equals(ner)) {
+            Utils.addAnn(outset, spanOffset+lastBegin, spanOffset+lastEnd, lastType, Utils.featureMap());
+            lastBegin = -1;
+            lastType = "O";
+          }
+          // if we currently do have a NE then if this is the first, remember the begin 
+          // always remember the end
+          if(!ner.equals("O")) {
+            if(lastBegin == -1) lastBegin = begin;
+            lastEnd = end;
+          }
+          // the last type is set always
+          lastType = ner;
+        }
+      }
+      // we processed a sentence, now create the sentence annotation
+      Utils.addAnn(outset, spanOffset+sentenceBegin, spanOffset+sentenceEnd, "Sentence", Utils.featureMap());
+      // also create an NE annotation if one is still outstanding
+      if(lastType != null && !lastType.equals("O")) {
+        Utils.addAnn(outset, spanOffset+lastBegin, spanOffset+lastEnd, ner, Utils.featureMap());
+      }
+    } // for sentence : sentences
+  } 
   
 
   @Override
